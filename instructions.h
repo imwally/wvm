4cf305825140956f57cb235d28f868f0daa66146
Wally Jones <wally@imwally.net>
2016-09-06 17:54:16 -0400

    Add instructions.h

---
+++ instructions.h
@@ -0,0 +1,18 @@
+/*
+ * instructions.h
+ *
+ * Define the instruction set for Wally's Virtual Machine.
+ */
+
+#ifndef WVM_INSTRUCTIONS_H
+#define WVM_INSTRUCTIONS_H
+
+enum operations {
+    HALT,
+    ADD,
+    SUB,
+    MUL,
+    DIV,
+};
+
+#endif
